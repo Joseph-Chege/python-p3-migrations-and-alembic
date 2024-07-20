caa565a7ba2a28df99821835ec08d11fd1501d92
Joseph-Chege <mjchege23@gmail.com>
2024-07-20 10:55:10 +0300

    Handled migrations using Alembic

---
+++ app/migrations/versions/60a60cde77b7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 60a60cde77b7
+Revises: 
+Create Date: 2024-07-20 10:23:55.638251
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '60a60cde77b7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
